2deddfd86d69f703ca03074d1c2eae5649fec380
VladiusVostokus <kovalenkov222@gmail.com>
2025-06-12 17:57:14 +0300

    Push an array

--- tests.js
+++ tests.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const assert = require('node:assert/strict');
+const assert = require('assert');
 const FixedQueue = require('./fixedQueue');
 
 // Simple correct usage
-const queue = new FixedQueue();
+let queue = new FixedQueue();
 
 queue.push(1);
 queue.push(2);
@@ -22,4 +22,20 @@ assert.equal(queue.shift(), null);
 // Doesn't change pushed elements inside
 queue.push(2);
 assert.notEqual(queue.shift(), 3);
+
+
+// Push an array
+queue = new FixedQueue();
+const testArr = [];
+const expectedArray = [];
+
+for (let i = 0; i < 10; i++) {
+  testArr[i] = i;
+  expectedArray[i] = i;
+}
+
+queue.push(testArr);
+assert.deepEqual(queue.shift(), expectedArray);
+
+
 
