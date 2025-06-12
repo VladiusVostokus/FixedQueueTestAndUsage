80a827e2712a480d496be619bb55606feb7424f0
VladiusVostokus <kovalenkov222@gmail.com>
2025-06-12 16:40:30 +0300

    Add simple correct usage example

---
+++ tests.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const assert = require('node:assert/strict');
+const FixedQueue = require('./fixedQueue');
+
+// Simple correct usage
+const queue = new FixedQueue();
+
+queue.push(1);
+queue.push(2);
+queue.push(3);
+
+assert(queue.shift(), 1);
+assert(queue.shift(), 2);
+assert(queue.shift(), 3);
+
+assert(queue.isEmpty, true);
+
