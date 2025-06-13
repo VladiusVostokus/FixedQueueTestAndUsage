0bea28a13ce936d6ec8c06b7fadc96c3036971df
VladiusVostokus <kovalenkov222@gmail.com>
2025-06-13 08:51:00 +0300

    Push 10000 elements

--- usage.js
+++ usage.js
@@ -22,3 +22,14 @@ console.log(queue.shift()); // [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
 queue.push("hello world");
 console.log(queue.shift()); // hello world
 
+let shiftSum = 0;
+for (let i = 0; i < 10000; i++) {
+  queue.push(1);
+}
+
+for (let i = 0; i < 10000; i++) {
+  shiftSum += queue.shift();
+}
+
+console.log(shiftSum); // 10000
+
